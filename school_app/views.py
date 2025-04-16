1cda1ce2746e9b9217fa6343fad2bd3abeb32b0e
AkobirovS <bayonxon@gmail.com>
2025-04-16 01:43:04 -0400

    modedy

--- school_app/views.py
+++ school_app/views.py
@@ -1,12 +1,20 @@
 from django.shortcuts import render
-from rest_framework.viewsets import ModelViewSet
-from .models import User
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import RegistrationSerializer
 from .serializer import UserSerializer
 
 
-class UserViewSet(ModelViewSet):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
+class RegistrationView(APIView):
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response( {'message':'sucsesful'}, status=status.HTTP_201_CREATED)
+        else:
+            return Response(self.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 def index(request):
     context = {}
